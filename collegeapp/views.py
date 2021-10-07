90f51065d90b8726dd22c80bcafc3eeffcf64dc3
unknown <maria.lbcs@icloud.com>
2021-10-07 09:05:21 -0400

    feat: changes views order

--- collegeapp/views.py
+++ collegeapp/views.py
@@ -12,12 +12,14 @@ admin = Admin(app,
               )
 
 
-admin.add_view(ModelView(Aluno, db.session))
-admin.add_view(ModelView(Curso, db.session))
 admin.add_view(ModelView(Professor, db.session))
+admin.add_view(ModelView(Curso, db.session))
+admin.add_view(ModelView(Aluno, db.session))
 admin.add_view(ModelView(Materia, db.session))
 admin.add_view(ModelView(MatriculadoEm, db.session))
 admin.add_view(ModelView(LecionadaPor, db.session))
 
 with app.app_context():
     event.listen(db.engine, 'connect', lambda con, rec: con.execute('pragma foreign_keys=ON'))
+
+
