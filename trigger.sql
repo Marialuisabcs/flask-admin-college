25a144df72b0845cd04a94d3cbd5ff1235b13da9
CaioUFMS <83556271+CaioUFMS@users.noreply.github.com>
2021-11-04 20:12:30 -0400

    Atualização dos triggers
    Alteração da function de inserção na tabela de logs, implementação de triggers para alterações ou deleções nas 4 tabelas principais (aluno, professor, materia e curso).

--- trigger.sql
+++ trigger.sql
@@ -1,14 +1,45 @@
-CREATE OR REPLACE FUNCTION test_log() RETURNS TRIGGER AS $$
+CREATE FUNCTION alteracaolog() RETURNS TRIGGER AS $$
+DECLARE
+tabela_alterada varchar(10) = TG_RELNAME;
+
+data_alteracao timestamp = CURRENT_DATE;
+
+operacao char = CASE 
+                 WHEN TG_OP = 'UPDATE' THEN 'U'
+                 WHEN TG_OP = 'DELETE' THEN 'D'
+                 END;
+
 BEGIN
-  INSERT INTO log (data, operacao) VALUES(CURRENT_DATE, 'D');
-  RETURN NULL;
+    INSERT INTO logs (tabela_alterada, nome, data_alteracao, operacao) VALUES(tabela_alterada, 
+                                                                              old.nome, 
+                                                                              data_alteracao,
+                                                                              operacao);
+return null;
+
 END;
 $$ LANGUAGE PLPGSQL;
 
-DROP TRIGGER IF EXISTS testlogs ON aluno;
 
-CREATE TRIGGER testlogs
-  AFTER DELETE OR UPDATE ON aluno
-  FOR EACH ROW
-  EXECUTE FUNCTION test_log();
+CREATE TRIGGER alunolog
+	BEFORE UPDATE OR DELETE ON aluno FOR EACH ROW
+	EXECUTE FUNCTION alteracaolog();
 
+CREATE TRIGGER cursolog
+	BEFORE UPDATE OR DELETE ON curso FOR EACH ROW
+	EXECUTE FUNCTION alteracaolog();
+
+CREATE TRIGGER professorlog
+	BEFORE UPDATE OR DELETE ON professor FOR EACH ROW
+	EXECUTE FUNCTION alteracaolog();
+
+CREATE TRIGGER materialog
+	BEFORE UPDATE OR DELETE ON materia FOR EACH ROW
+	EXECUTE FUNCTION alteracaolog();
+
+ALTER TABLE aluno ENABLE TRIGGER alunolog;
+
+ALTER TABLE curso ENABLE TRIGGER cursolog;
+
+ALTER TABLE professor ENABLE TRIGGER professorlog;
+
+ALTER TABLE materia ENABLE TRIGGER materialog;
