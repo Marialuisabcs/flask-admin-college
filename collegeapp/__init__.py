a5d78470c011e3866428cdbe066dea05923a6cae
unknown <maria.lbcs@icloud.com>
2021-11-08 16:28:48 -0400

    fix: changes databse_uri

--- collegeapp/__init__.py
+++ collegeapp/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config[
-    'SQLALCHEMY_DATABASE_URI'] = "postgres://ckkiyfgygbrzhw:4322377416b4cd6eef6fca8d241fad52e8b856ac8bedfb3f177a9a050859882c@ec2-54-147-207-184.compute-1.amazonaws.com:5432/da8gi3mhaqotkc"
+    'SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://ckkiyfgygbrzhw:4322377416b4cd6eef6fca8d241fad52e8b856ac8bedfb3f177a9a050859882c@ec2-54-147-207-184.compute-1.amazonaws.com:5432/da8gi3mhaqotkc"
 app.config['SECRET_KEY'] = '567041680499569'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['BABEL_DEFAULT_LOCALE'] = "PT"
