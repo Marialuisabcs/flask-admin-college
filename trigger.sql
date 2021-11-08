a8fd80203f056f92b547134cfd506e3920f4e593
unknown <maria.lbcs@icloud.com>
2021-11-08 15:41:45 -0400

    fix: change function, switch triggers to after op and adds necessary drops

--- trigger.sql
+++ trigger.sql
@@ -1,45 +1,37 @@
-CREATE FUNCTION alteracaolog() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION alteracaolog() RETURNS TRIGGER AS $$
 DECLARE
-tabela_alterada varchar(10) = TG_RELNAME;
-
-data_alteracao timestamp = CURRENT_DATE;
-
-operacao char = CASE 
+operacao char = CASE
                  WHEN TG_OP = 'UPDATE' THEN 'U'
                  WHEN TG_OP = 'DELETE' THEN 'D'
                  END;
 
 BEGIN
-    INSERT INTO logs (tabela_alterada, nome, data_alteracao, operacao) VALUES(tabela_alterada, 
-                                                                              old.nome, 
-                                                                              data_alteracao,
+    INSERT INTO log (tabela_alterada, nome, data_alteracao, operacao) VALUES(TG_RELNAME,
+                                                                              old.nome,
+                                                                              CURRENT_DATE,
                                                                               operacao);
-return null;
-
+    RETURN NULL;
 END;
 $$ LANGUAGE PLPGSQL;
 
+DROP TRIGGER IF EXISTS alunolog ON aluno;
+DROP TRIGGER IF EXISTS cursolog ON curso;
+DROP TRIGGER IF EXISTS professorlog ON professor;
+DROP TRIGGER IF EXISTS materialog ON materia;
 
 CREATE TRIGGER alunolog
-	BEFORE UPDATE OR DELETE ON aluno FOR EACH ROW
+	AFTER UPDATE OR DELETE ON aluno FOR EACH ROW
 	EXECUTE FUNCTION alteracaolog();
 
 CREATE TRIGGER cursolog
-	BEFORE UPDATE OR DELETE ON curso FOR EACH ROW
+	AFTER UPDATE OR DELETE ON curso FOR EACH ROW
 	EXECUTE FUNCTION alteracaolog();
 
 CREATE TRIGGER professorlog
-	BEFORE UPDATE OR DELETE ON professor FOR EACH ROW
+	AFTER UPDATE OR DELETE ON professor FOR EACH ROW
 	EXECUTE FUNCTION alteracaolog();
 
 CREATE TRIGGER materialog
-	BEFORE UPDATE OR DELETE ON materia FOR EACH ROW
+	AFTER UPDATE OR DELETE ON materia FOR EACH ROW
 	EXECUTE FUNCTION alteracaolog();
 
-ALTER TABLE aluno ENABLE TRIGGER alunolog;
-
-ALTER TABLE curso ENABLE TRIGGER cursolog;
-
-ALTER TABLE professor ENABLE TRIGGER professorlog;
-
-ALTER TABLE materia ENABLE TRIGGER materialog;
