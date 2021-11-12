65a8f332ad3c24fb58b7b67e8e5f58d00d00ddaa
unknown <maria.lbcs@icloud.com>
2021-11-12 18:17:43 -0400

    fix: changes triggers

--- trigger.sql
+++ trigger.sql
@@ -1,1 +1,7 @@
-drop table if exists al;
+drop table if exists matriculadoEm CASCADE;
+drop table if exists lacionadoPor CASCADE;
+drop table if exists professor CASCADE;
+drop table if exists curso CASCADE;
+drop table if exists materia CASCADE;
+drop table if exists aluno CASCADE;
+drop table if exists log CASCADE;
