8cba69fa5e5c88ef8deb7b32491dbe32e5296998
unknown <maria.lbcs@icloud.com>
2021-11-12 18:13:16 -0400

    feat: adds session maker

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from collegeapp.models import *
 from sqlalchemy import text
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+
+engine = create_engine(app.config.get('SQLALCHEMY_DATABASE_URI'))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 db.create_all()
 db.session.commit()
 
@@ -10,3 +15,5 @@ result = db.engine.execute(sql)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
