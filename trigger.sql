d7b76c30f5afccae41e4c655cfb839b236e2e98e
unknown <maria.lbcs@icloud.com>
2021-11-12 17:23:46 -0400

    fix: changes drop statement

--- trigger.sql
+++ trigger.sql
@@ -5,3 +5,10 @@ drop table if exists curso CASCADE;
 drop table if exists materia CASCADE;
 drop table if exists aluno CASCADE;
 drop table if exists log CASCADE;
+
+DROP TRIGGER IF EXISTS alunolog ON aluno;
+DROP TRIGGER IF EXISTS cursolog ON curso;
+DROP TRIGGER IF EXISTS professorlog ON professor;
+DROP TRIGGER IF EXISTS materialog ON materia;
+
+drop function alteracaolog();
