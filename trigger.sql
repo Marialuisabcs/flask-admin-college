3a3232c90a02150c05c9fc9eebd802f5f7a99a79
unknown <maria.lbcs@icloud.com>
2021-11-12 17:31:10 -0400

    fix: changes triggers

--- trigger.sql
+++ trigger.sql
@@ -1,3 +1,17 @@
+drop table if exists matriculadoEm CASCADE;
+drop table if exists lacionadoPor CASCADE;
+drop table if exists professor CASCADE;
+drop table if exists curso CASCADE;
+drop table if exists materia CASCADE;
+drop table if exists aluno CASCADE;
+drop table if exists log CASCADE;
+
+DROP TRIGGER IF EXISTS alunolog ON aluno;
+DROP TRIGGER IF EXISTS cursolog ON curso;
+DROP TRIGGER IF EXISTS professorlog ON professor;
+DROP TRIGGER IF EXISTS materialog ON materia;
+
+
 CREATE OR REPLACE FUNCTION alteracaolog() RETURNS TRIGGER AS $$
     DECLARE
         op char = CASE
@@ -10,11 +24,6 @@ CREATE OR REPLACE FUNCTION alteracaolog() RETURNS TRIGGER AS $$
         RETURN NULL;
 END;
 $$ LANGUAGE PLPGSQL;
-
-DROP TRIGGER IF EXISTS alunolog ON aluno;
-DROP TRIGGER IF EXISTS cursolog ON curso;
-DROP TRIGGER IF EXISTS professorlog ON professor;
-DROP TRIGGER IF EXISTS materialog ON materia;
 
 CREATE TRIGGER alunolog
 	AFTER UPDATE OR DELETE ON aluno FOR EACH ROW
