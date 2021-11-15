b3b754d477343c234edaeece7a45bc8ebaece945
unknown <maria.lbcs@icloud.com>
2021-11-15 13:54:55 -0400

    fix: changes commit statement

--- app.py
+++ app.py
@@ -3,20 +3,14 @@ from sqlalchemy import text
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 engine = create_engine(app.config.get('SQLALCHEMY_DATABASE_URI'))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 db.create_all()
+db.session.commit()
 
 with open('trigger.sql', 'r') as f:
     sql = text(f.read())
 result = db.engine.execute(sql)
 
-db.session.commit()
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-
