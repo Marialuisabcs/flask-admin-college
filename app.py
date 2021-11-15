f06beac092d8afdd4c2982a2d15eafd3c8b93d32
unknown <maria.lbcs@icloud.com>
2021-11-15 14:05:13 -0400

    fix: changes sessions

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from collegeapp.models import *
 from sqlalchemy import text
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 engine = create_engine(app.config.get('SQLALCHEMY_DATABASE_URI'))
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Session = scoped_session(sessionLocal)
+
+session = Session()
 db.create_all()
 db.session.commit()
+Session.remove()
+
+session_trigger = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Session_trigger = scoped_session(session_trigger)
+
+session_trigger = Session()
 
 with open('trigger.sql', 'r') as f:
     sql = text(f.read())
 result = db.engine.execute(sql)
+
+Session_trigger.remove()
 
 if __name__ == '__main__':
     app.run(debug=True)
