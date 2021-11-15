6c5ff0730abb9b2844ac8bbfbe17aa27dad75907
unknown <maria.lbcs@icloud.com>
2021-11-15 13:43:38 -0400

    fix: changes database

--- collegeapp/__init__.py
+++ collegeapp/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config[
-    'SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://ckkiyfgygbrzhw:4322377416b4cd6eef6fca8d241fad52e8b856ac8bedfb3f177a9a050859882c@ec2-54-147-207-184.compute-1.amazonaws.com:5432/da8gi3mhaqotkc"
+    'SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://ygjqxvmmslyfod:0cefce656bf0f55ec4c070c19a9a249c556bd3866d64d1be0cf2d67b069229a0@ec2-34-203-114-67.compute-1.amazonaws.com:5432/d1sgr0fu0rbh6r"
 app.config['SECRET_KEY'] = '567041680499569'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['BABEL_DEFAULT_LOCALE'] = "PT"
